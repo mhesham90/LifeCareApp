561231126f97fa1c2ba550f7a5a86c0fa29c3148
mhesham90 <mohammedhesham.1990@gmail.com>
2017-05-19 18:52:54 +0200

    added test controller

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,8 @@
 var functions = require('firebase-functions');
-
-
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const testModule = require('./controllers/testController');
 
-module.exports.test = functions.https.onRequest((req, res) => {
-  admin.database().ref('/test').push({testKey: 'testValue'}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
-});
+
+exports.test = functions.https.onRequest(testModule.test);
